c5683d2a2a5eed6a3ac4b78149469856ec2fdd29
angelo-perez <92904983+angelo-perez@users.noreply.github.com>
2022-10-27 23:04:01 +0800

    Added Recipe Page Indicator

--- lib/recipes_categories/chicken_recipes.dart
+++ lib/recipes_categories/chicken_recipes.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../pages/recipes_page.dart';
 import '../main.dart';
 
@@ -16,7 +17,7 @@ class ChickenRecipes extends StatefulWidget {
 
 class _ChickenRecipesState extends State<ChickenRecipes> {
   int _index = 0;
-
+  PageController _controller = PageController(viewportFraction: 0.75);
   final CollectionReference _chickenRecipes =
       FirebaseFirestore.instance.collection('chicken-recipe');
 
@@ -28,32 +29,54 @@ class _ChickenRecipesState extends State<ChickenRecipes> {
           stream: _chickenRecipes.snapshots(),
           builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
             if (streamSnapshot.hasData) {
-              return Center(
-                child: SizedBox(
-                  height: 525, // card height
-                  child: PageView.builder(
-                    itemCount: streamSnapshot.data!.docs.length,
-                    controller:
-                        PageController(viewportFraction: 0.75), //card width
-                    onPageChanged: (int index) =>
-                        setState(() => _index = index),
-                    itemBuilder: (context, index) {
-                      final DocumentSnapshot documentSnapshot =
-                          streamSnapshot.data!.docs[index];
-                      return Transform.scale(
-                        scale: index == _index ? 1 : 0.85,
-                        child: Card(
-                          elevation: 8,
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 525, // card height
+                    child: PageView.builder(
+                      itemCount: streamSnapshot.data!.docs.length,
+                      controller: _controller, //card width
+                      onPageChanged: (int index) =>
+                          setState(() => _index = index),
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot documentSnapshot =
+                            streamSnapshot.data!.docs[index];
+                        return Transform.scale(
+                          scale: index == _index ? 1 : 0.85,
                           child: Card(
-                            elevation: 6,
-                            child: _chickenRecipeListItem(
-                                context, documentSnapshot),
+                            elevation: 8,
+                            child: Card(
+                              elevation: 6,
+                              child: _chickenRecipeListItem(
+                                  context, documentSnapshot),
+                            ),
                           ),
-                        ),
-                      );
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                  ),
+                  SmoothPageIndicator(
+                    controller: _controller,
+                    count: streamSnapshot.data!.docs.length,
+                    effect: ScaleEffect(
+                      activePaintStyle: PaintingStyle.stroke,
+                      scale: 1.7,
+                      activeStrokeWidth: 1.5,
+                      dotHeight: 10.0,
+                      dotWidth: 10.0,
+                      dotColor: Color(0xFF12A2726),
+                      activeDotColor: Color(0xFF12A2726),
+                    ),
+                    onDotClicked: (index) {
+                      //setState(() => _index = index);
                     },
                   ),
-                ),
+                ]
               );
             }
             return const Center(
