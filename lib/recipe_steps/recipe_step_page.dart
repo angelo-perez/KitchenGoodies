9d9495bb205ed7e74c57441d56304b7b2e44ba25
angelo-perez <92904983+angelo-perez@users.noreply.github.com>
2023-01-15 00:42:47 +0800

    Text-To-Speech is implemented in Recipe Step Page

--- lib/recipe_steps/recipe_step_page.dart
+++ lib/recipe_steps/recipe_step_page.dart
@@ -6,26 +6,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import '../util/colors.dart';
 import '../recipes_page/finished_page.dart';
 
 class RecipeStepPage extends StatefulWidget {
   RecipeStepPage(
-    this.recipeId,
-    this.collection_name,
-    this.recipe_image,
-    this.recipe_name,
-    this.recipe_source,
-    this.steps,
-    this.steps_timer,
-    this.total_steps_count,
-    this.step_index,
-    this.current_step,
-    this.current_step_duration,
-    this.recipe_rating,
-    this.recipe_type
-  );
+      this.recipeId,
+      this.collection_name,
+      this.recipe_image,
+      this.recipe_name,
+      this.recipe_source,
+      this.steps,
+      this.steps_timer,
+      this.total_steps_count,
+      this.step_index,
+      this.current_step,
+      this.current_step_duration,
+      this.recipe_rating,
+      this.recipe_type);
 
   String recipeId;
   String collection_name;
@@ -45,7 +45,18 @@ class RecipeStepPage extends StatefulWidget {
   State<RecipeStepPage> createState() => _RecipeStepPageState();
 }
 
+bool _isTtsEnabled = true;
+
 class _RecipeStepPageState extends State<RecipeStepPage> {
+  final FlutterTts stepTextToSpeech = FlutterTts();
+
+  speak(int stepNumber, String step) async {
+    await stepTextToSpeech.setLanguage("en-US");
+    await stepTextToSpeech.setPitch(1); //voice pitch of the speaker
+    await stepTextToSpeech.setSpeechRate(0.5);
+    await stepTextToSpeech.speak("Step ${stepNumber}, ${step}");
+  }
+
   @override
   Widget build(BuildContext context) {
     Color iconTextColor = Colors.white;
@@ -58,18 +69,35 @@ class _RecipeStepPageState extends State<RecipeStepPage> {
               content: new Text('Do you want to exit the recipe\'s procedure?'),
               actions: <Widget>[
                 TextButton(
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateProperty.all(
+                      Color.fromARGB(51, 42, 39, 38),
+                    ),
+                  ),
                   onPressed: () {
-                    FlutterRingtonePlayer.stop();
+                    // FlutterRingtonePlayer.stop();
+                    stepTextToSpeech.stop();
                     for (int i = widget.step_index; i >= 0; i--) {
                       print(widget.step_index);
                       Navigator.of(context).pop(true);
                     }
                   },
-                  child: new Text('Yes'),
+                  child: new Text(
+                    'Yes',
+                    style: TextStyle(color: mPrimaryColor),
+                  ),
                 ),
                 TextButton(
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateProperty.all(
+                      Color.fromARGB(51, 42, 39, 38),
+                    ),
+                  ),
                   onPressed: () => Navigator.of(context).pop(false),
-                  child: new Text('No'),
+                  child: new Text(
+                    'No',
+                    style: TextStyle(color: mPrimaryColor),
+                  ),
                 ),
               ],
             ),
@@ -81,9 +109,15 @@ class _RecipeStepPageState extends State<RecipeStepPage> {
       widget.step_index++;
     }
 
-    setState(() {
-      FlutterRingtonePlayer.stop();
-    });
+    // setState(() {
+    //   FlutterRingtonePlayer.stop();
+    // });
+
+    if (_isTtsEnabled) {
+      speak(widget.step_index, widget.current_step);
+    } else {
+      stepTextToSpeech.stop();
+    }
 
     return WillPopScope(
       onWillPop: _onWillPop,
@@ -91,12 +125,30 @@ class _RecipeStepPageState extends State<RecipeStepPage> {
         appBar: AppBar(
           backgroundColor: appBarColor,
           leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: mBackgroundColor,
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: mBackgroundColor,
+            ),
+            onPressed: () => _onWillPop(),
           ),
-          onPressed: () => _onWillPop(),
-        ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  widget.step_index--;
+                  _isTtsEnabled = !_isTtsEnabled;
+                  if (_isTtsEnabled) {
+                    speak(widget.step_index, widget.current_step);
+                  } else {
+                    stepTextToSpeech.stop();
+                  }
+                });
+              },
+              icon: _isTtsEnabled
+                  ? Icon(Icons.spatial_audio_off)
+                  : Icon(Icons.spatial_audio_off_outlined),
+            ),
+          ],
           title: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -163,8 +215,18 @@ class _RecipeStepPageState extends State<RecipeStepPage> {
                         widget.step_index > 1
                             ? IconButton(
                                 iconSize: 50,
-                                onPressed: () =>
-                                    Navigator.of(context).pop(true),
+                                onPressed: () {
+                                  widget.step_index--;
+                                  stepTextToSpeech.stop();
+                                  if (_isTtsEnabled) {
+                                    speak(
+                                        widget.step_index,
+                                        widget.steps[widget.steps
+                                                .indexOf(widget.current_step) -
+                                            1]);
+                                  }
+                                  Navigator.of(context).pop(true);
+                                },
                                 icon: Icon(
                                   FluentIcons.arrow_circle_left_28_filled,
                                   color: iconTextColor,
@@ -181,27 +243,30 @@ class _RecipeStepPageState extends State<RecipeStepPage> {
                         widget.step_index < widget.steps.length
                             ? IconButton(
                                 iconSize: 50,
-                                onPressed: () => pushNewScreen(
-                                  context,
-                                  screen: RecipeStepPage(
-                                    widget.recipeId,
-                                    widget.collection_name,
-                                    widget.recipe_image,
-                                    widget.recipe_name,
-                                    widget.recipe_source,
-                                    widget.steps,
-                                    widget.steps_timer,
-                                    widget.total_steps_count,
-                                    widget.step_index,
-                                    widget.steps[widget.step_index],
-                                    widget.steps_timer[widget.step_index],
-                                    widget.recipe_rating,
-                                    widget.recipe_type
-                                  ),
-                                  withNavBar: false,
-                                  pageTransitionAnimation:
-                                      PageTransitionAnimation.fade,
-                                ),
+                                onPressed: () {
+                                  stepTextToSpeech.stop();
+
+                                  pushNewScreen(
+                                    context,
+                                    screen: RecipeStepPage(
+                                        widget.recipeId,
+                                        widget.collection_name,
+                                        widget.recipe_image,
+                                        widget.recipe_name,
+                                        widget.recipe_source,
+                                        widget.steps,
+                                        widget.steps_timer,
+                                        widget.total_steps_count,
+                                        widget.step_index,
+                                        widget.steps[widget.step_index],
+                                        widget.steps_timer[widget.step_index],
+                                        widget.recipe_rating,
+                                        widget.recipe_type),
+                                    withNavBar: false,
+                                    pageTransitionAnimation:
+                                        PageTransitionAnimation.fade,
+                                  );
+                                },
                                 icon: Icon(
                                   FluentIcons.arrow_circle_right_28_filled,
                                   color: iconTextColor,
@@ -210,11 +275,13 @@ class _RecipeStepPageState extends State<RecipeStepPage> {
                             : IconButton(
                                 iconSize: 50,
                                 onPressed: () {
+                                  stepTextToSpeech.stop();
+
                                   for (int i = widget.step_index; i >= 0; i--) {
                                     print(widget.step_index);
                                     Navigator.of(context).pop(true);
                                   }
-                                  FlutterRingtonePlayer.stop;
+                                  // FlutterRingtonePlayer.stop;
                                   pushNewScreen(
                                     context,
                                     screen: FinishedRecipePage(
